0940ac9c0c6f1c23d8b51585e6024ab24937b652
lee <dbdks32123@gmail.com>
2024-03-20 20:08:34 +0900

    Contact section

---
+++ src/components/Contact.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from '@/styles/contact.module.scss'
+
+const Contact = () => {
+  return (
+    <div id="contact">
+        <div className={styles.main}>
+            <div>
+            CONTACT
+            </div>
+            <div>
+                <div><div></div>이름 <br></br>이유안</div>
+                <div><div></div>생년월일<br></br>96.08.19</div>
+                <div><div></div>연락처<br></br>010-5526-0787</div>
+                <div><div></div>메일<br></br>dbdks32123@gmail.com</div>
+            </div>
+            </div>
+    </div>
+  );
+};
+
+export default Contact;
