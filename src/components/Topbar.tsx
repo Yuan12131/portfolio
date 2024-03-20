593ece05dfdad1f0d68c409be575e2805f6071c4
lee <dbdks32123@gmail.com>
2024-03-20 20:09:04 +0900

    Topbar

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "@/styles/topbar.module.scss";
+
+
+function Topbar() {
+  const [activeSection, setActiveSection] = useState("");
+  const [showBackground, setShowBackground] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ["contact", "Archiving", "project", "stack"];
+      const currentSection = sections.find((section) => {
+        const element = document.getElementById(section);
+        return element.getBoundingClientRect().top < window.innerHeight / 2;
+      });
+      setActiveSection(currentSection || "");
+    };
+
+    const handleScrollTop = () => {
+      setShowBackground(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScrollTop);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScrollTop);
+    };
+  }, []);
+
+  return (
+    <div className={`${styles.topbar} ${showBackground ? styles.background : ''}`}>
+      <div>
+        <Link href="/">LYA'S Portfolio</Link>
+      </div>
+      <div>
+        <Link href="#contact">Contact</Link>
+        <Link href="#archiving">Archiving</Link>
+        <Link href="#project">Project</Link>
+        <Link href="#stack">Stack</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
