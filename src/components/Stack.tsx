fdd35761019e8f9a8a31cbdd98858bdf0cb7e10e
lee <dbdks32123@gmail.com>
2024-03-22 16:57:52 +0900

    Stack Component

---
+++ src/components/Stack.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "@/styles/stack.module.scss";
+
+const Stack = () => {
+  return (
+    <div id="stack">
+      <div className={styles.main}>
+        <div>STACK</div>
+        <div>
+          <div></div>
+            <div></div>
+            <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stack;
