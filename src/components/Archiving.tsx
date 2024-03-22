7b595162e585a01ae2a1cda6dd61ceea92df6c43
lee <dbdks32123@gmail.com>
2024-03-22 18:15:21 +0900

    Archiving Style 수정

--- src/components/Archiving.tsx
+++ src/components/Archiving.tsx
@@ -9,27 +9,30 @@ const Archiving = () => {
         <div>ARCHIVING</div>
         <div>
           <div>
-          <div>
-            <div></div>
-            <div>Github</div>
-            <Link href={"https://github.com/Yuan12131"}>이동하기</Link>
-          </div>
+            <div><Link href={"https://github.com/Yuan12131"}>
+              <div></div>
+              <span>Github</span>
+              <span>&nbsp;&nbsp;👉&nbsp;&nbsp;이동하기</span></Link>
+            </div>
             <div>- 작업한 모든 프로젝트의 소스코드</div>
             <div>- 코딩 연습용으로 작성한 소스코드</div>
             <div>- 교육 과정 중 작성한 소스코드</div>
           </div>
           <div>
             <div>
+              <Link
+                href={
+                  "https://ubiquitous-ravioli-eff.notion.site/ba88aea188ab4d718edb14f9e8138fd7?v=e841ca3e13d7404fbe02cc84a5cdb922"
+                }
+              >
                 <div></div>
-              <div>Notion</div>
-            <Link href={"https://ubiquitous-ravioli-eff.notion.site/ba88aea188ab4d718edb14f9e8138fd7?v=e841ca3e13d7404fbe02cc84a5cdb922"}>이동하기</Link>
-
+                <span>Notion</span>
+                <span>&nbsp;&nbsp;👉&nbsp;&nbsp;이동하기</span>
+              </Link>
             </div>
-              <div>- 교육 과정 중 습득한 지식 정리</div>
-              <div>- 혼자 연구한 기술 및 개념 기록</div>
-              <div>- 작업한 프로젝트의 기록</div>
-
-
+            <div>- 교육 과정 중 습득한 지식 정리</div>
+            <div>- 혼자 연구한 기술 및 개념 기록</div>
+            <div>- 작업한 프로젝트의 기록</div>
           </div>
         </div>
       </div>
