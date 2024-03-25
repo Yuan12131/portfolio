78f0167c798209fa0ae96263406c9c578906945a
lee <dbdks32123@gmail.com>
2024-03-25 22:44:02 +0900

    Stack Component

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -7,9 +7,210 @@ const Stack = () => {
       <div className={styles.main}>
         <div>STACK</div>
         <div>
-          <div></div>
-            <div></div>
-            <div></div>
+          <div>BACKEND</div>
+          <div>
+            <div>
+              <div>
+                <div></div>
+                <div>Node.js(Express)</div>
+              </div>
+              <div>
+                서버 구축, HTTP 요청 처리, AJAX를 활용한 비동기 서버 통신을 통한
+                동적인 데이터 업데이트를 수행했습니다.
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+                <div>MySQL, MongoDB</div>
+              </div>
+              <div>
+                CRUD 작업을 수행하며 데이터베이스의 구조와
+                테이블 간의 관계를 설계하고, 쿼리를 작성하여 데이터를
+                관리했습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>Rest API</div>
+              </div>
+              <div>
+                REST API를 활용하여 클라이언트 애플리케이션과 서버 간의 통신을
+                구현하고, 데이터를 전송하고 처리한 경험이 있습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>AWS EC2</div>
+              </div>
+              <div>
+                호스팅 후 로드 밸런서를 설정하고, SSL 인증서를 통해 서버
+                보안을 강화한 바 있습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>AWS Lightsail</div>
+              </div>
+              <div>
+                가상 서버를 구축해 호스팅하고, 데이터베이스를 연결했습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>AWS Route53</div>
+              </div>
+              <div>
+                도메인을 등록하고 DNS 설정을 구성하였으며, 필요에 따라 레코드를
+                관리했습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>FIREBASE</div>
+              </div>
+              <div>
+                Firebase CLI를 통해 프로젝트를 로컬 환경에서 테스트하고
+                배포했습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+                
+              <div></div>
+              <div>Vercel</div>
+              </div>
+              <div>
+              프로젝트의 관리 및 유지보수를 위해 자동화된 기능을 활용하여 신속하게 배포했습니다.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>FRONTEND</div>
+          <div>
+            <div>
+              <div>
+                <div></div>
+                <div>Javascript (ES6+)</div>
+              </div>
+              <div>
+              ESM과 CJS 모듈 시스템, 그리고 화살표 함수 같은 함수 작성법들을 숙지하고 있습니다. 또한, 인터프리터 언어적 특성과 동작 원리에 대해 공부하고, 동기와 비동기 프로그래밍에 대한 이해를 높이고 있습니다.
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+                <div>Typescript</div>
+              </div>
+              <div>
+              타입 관리를 통해 코드의 안정성을 높여 프로젝트를 진행해보았습니다. 특히, props를 통해 컴포넌트 간의 데이터 흐름을 타입 안전하게 관리하는 것에 대해 연구하고 있습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div> Next.js</div>
+              </div>
+              <div>
+              Node.js 서버와의 통합이나 서버리스 아키텍처를 구축한 경험이 있습니다. 특히, Route API를 활용하여 SSR과 CSR의 차이를 연구하고 프로젝트에 적용하는 연습을 하고 있습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>React</div>
+              </div>
+              <div>
+              컴포넌트 기반으로 UI를 모듈화하고 재사용 가능한 구성 요소로 만들어 관리하였습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>HTML5</div>
+              </div>
+              <div>
+              컨테이너 레벨과 아이템 레벨의 차이를 이해하고, 적절한 마크업을 구현했습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>css3, scss</div>
+              </div>
+              <div>
+              선택자를 효과적으로 활용하여 특정 요소에 스타일을 적용하고, 관계형 선택자를 이용하여 요소 간의 관계에 따라 스타일을 조정하였습니다.
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+        <div>
+          <div>TOOL</div>
+          <div>
+            <div>
+              <div>
+                <div></div>
+                <div>Git/ GitHub</div>
+              </div>
+              <div>
+              CLI 방식으로 브랜치를 생성하고 관리하며 push와 pull request를 활용하여 협업 한 경험이 있습니다.
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+                <div>Figma</div>
+              </div>
+              <div>
+              와이어프레임을 제작하고, 인터랙션 기능을 이용한 프로토타입을 구현하며 사용자 경험을 시뮬레이션하였습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div> VSCODE</div>
+              </div>
+              <div>
+            Remote SSH, Prettier 등 다양한 확장 프로그램을 통해 개발 툴을 확장하고, 테마와 단축키를 설정하여 개발 효율을 높였습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>Notion</div>
+              </div>
+              <div>
+              프로젝트 일정, 작업 목록, 문서 작성 등 Notion을 활용한 프로젝트 관리 및 문서를 작성하고 있습니다.
+              </div>
+            </div>{" "}
+            <div>
+              <div>
+
+              <div></div>
+              <div>Slack</div>
+              </div>
+              <div>
+              커뮤니케이션 및 협업을 위해 Slack을 사용했습니다.
+              </div>
+            </div>{" "}
+          </div>
         </div>
       </div>
     </div>
