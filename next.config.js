6e9ea3e963d2d03fcb0beef8cae0bb8cf1b6b391
lee <dbdks32123@gmail.com>
2024-04-04 17:00:09 +0900

    타입 에러 해결 - 주석 처리

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-module.exports = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  }
+// module.exports = {
+//     typescript: {
+//       ignoreBuildErrors: true,
+//     },
+//   }
